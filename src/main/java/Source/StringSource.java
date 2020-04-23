c852231003d9cccff2c2d7655fb7b77c3885ad0c
ekowalsk <ewcia.kowalska@gmail.com>
2020-04-23 19:53:14 +0200

    added StringSOurce class

---
+++ src/main/java/Source/StringSource.java
@@ -0,0 +1,33 @@
+package Source;
+
+public class StringSource extends Source {
+    private String source;
+    private boolean isEnd;
+    private int positionInString;
+
+    public StringSource (String source) {
+        currentRow = 1;
+        currentColumn = 0;
+        this.source = source;
+        isEnd = false;
+        positionInString = 0;
+        consume();
+    }
+    @Override
+    public boolean consume() {
+        if (isEnd)
+            return false;
+        getNextChar();
+        updateCoordinates();
+        return true;
+    }
+    public boolean isEnd() { return isEnd; }
+    private void getNextChar() {
+        if (positionInString < source.length()) {
+            currentChar = source.charAt(positionInString);
+            positionInString++;
+        }
+        else
+            isEnd = true;
+    }
+}
