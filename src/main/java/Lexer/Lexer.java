63012273fc41048754c5e132cc94a7b30daf53a8
ekowalsk <ewcia.kowalska@gmail.com>
2020-04-25 08:34:15 +0200

    started implementing of Lexer class

--- src/main/java/Lexer/Lexer.java
+++ src/main/java/Lexer/Lexer.java
@@ -1,4 +1,10 @@
 package Lexer;
 
+import Source.Source;
+
 public class Lexer {
+    private Source source;
+    private StringBuilder lexeme;
+    private int startRow;
+    private int startColumn;
 }
