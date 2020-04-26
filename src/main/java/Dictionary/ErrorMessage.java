7ff0114640eaea35ee12e672d09cd16fe7bbcdcc
ekowalsk <ewcia.kowalska@gmail.com>
2020-04-26 23:21:51 +0200

    added method for skipping whitespace and comments and for getting identifiers or keywords

---
+++ src/main/java/Dictionary/ErrorMessage.java
@@ -0,0 +1,7 @@
+package Dictionary;
+
+public class ErrorMessages {
+    public final String IDENTIFIER_EXPECTED = " identifier expected: ";
+    public final String ASSIGNOP_EXPECTED = " ':=' expected: ";
+    public final String TYPE_EXPECTED = 
+}
--- src/main/java/Lexer/Lexer.java
+++ src/main/java/Lexer/Lexer.java
@@ -1,10 +1,62 @@
 package Lexer;
 
+import Dictionary.ErrorMessage;
+import Dictionary.Keywords;
+import Dictionary.LexemeType;
 import Source.Source;
+import java.awt.Point;
 
 public class Lexer {
     private Source source;
     private StringBuilder lexeme;
     private int startRow;
     private int startColumn;
+    private LexemeType type;
+    private Token token;
+    private final int MAX_IDENT_LEN = 30;
+
+    public Lexer (Source source) {
+        this.source = source;
+        //consume();
+    }
+
+    public Token getToken() { return token; }
+    private void setToken() {
+        token = new Token (type, lexeme.toString(), new Point(startRow, startColumn));
+    }
+    private void skipWhiteSpacesAndComments() {
+        while (Character.isWhitespace(source.getCurrentChar()) || source.getCurrentChar() == ';'){
+            if (source.getCurrentChar() == ';')
+                skipComments();
+            else
+                source.consume();
+        }
+    }
+    private void skipComments() {
+        if (source.getCurrentChar() == ';') {
+            while (source.getCurrentChar() != '\n')
+                source.consume();
+            source.consume();
+        }
+    }
+    private void getIdentifierOrKeyword() throws IllegalArgumentException {
+        while (Character.isLetterOrDigit(source.getCurrentChar()) || source.getCurrentChar() == '_'){
+            if (lexeme.length() > MAX_IDENT_LEN)
+                throw new IllegalArgumentException(getMessage(ErrorMessage.IDENTIFIER_SIZE_EXCEEDED));
+            lexeme.append(source.getCurrentChar());
+            source.consume();
+        }
+        setLexemeType();
+    }
+    private void setLexemeType() {
+        type = Keywords.getLexemeType(lexeme.toString());
+        if (type.equals(LexemeType.UNDEFINED))
+            type = Dictionary.LexemeType.IDENT;
+    }
+    private Point getPosition() {
+        return new Point(startRow, startColumn);
+    }
+    private String getMessage(String message){
+        return message + "" + getPosition() + ")\n";
+    }
 }
