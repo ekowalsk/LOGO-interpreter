a1508b936e1d070a5a47b512e57764aef44a4ba6
ekowalsk <ewcia.kowalska@gmail.com>
2020-04-28 20:25:01 +0200

    added more error messages

--- src/main/java/Dictionary/ErrorMessage.java
+++ src/main/java/Dictionary/ErrorMessage.java
@@ -5,4 +5,6 @@ public class ErrorMessage {
     public static final String ASSIGNOP_EXPECTED = " ':=' expected: ";
     public static final String TYPE_EXPECTED = " type expected: ";
     public static final String IDENTIFIER_SIZE_EXCEEDED = " maximum identifier size exceed: ";
+    public static final String BAD_NUMBER = " bad number: ";
+    public static final String STRING_SIZE_EXCEEDED = " maximum string size exceeded: ";
 }
